d82cc01e0713e50e182b812c9de0ee8f0619af8a
zgoodman27 <zgoodman27@gmail.com>
2025-07-11 00:13:22 -0400

    finalized project

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 
 const PORT = 3000;
 
+// check if the server is running
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+// get answers from the JSON file
+app.get("/answers", (req, res) => {
+    res.sendFile(__dirname + '/api/answers.json');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 });
